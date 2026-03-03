070b661cfd14735302466e546b8ded7fbacf3fc0
Javier Espitia <javi.espitiad@outlook.com>
2026-03-03 12:23:00 -0500

    creates GifList component

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,4 +1,6 @@
-import { mockGifs } from "./mock-data/gifs.mock";
+import { GifList } from "./gifs/components/GifList";
+import { mockGifs } from './mock-data/gifs.mock';
+import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 
@@ -16,32 +18,12 @@ export const GifsApp = () => {
 
       {/* Previous search */}
 
-      <div className="previous-searches">
-        <h2>Busquedas previas</h2>
-        <ul className="previous-searches-list">
-          <li>Goku</li>
-          <li>Bubu</li>
-          <li>Futbol</li>
-        </ul>
-      </div>
+      <PreviousSearches searches={['Goku', 'Dragon Ball']}/>
 
       { /* Gifs */ }
 
+      <GifList gifs={mockGifs} />
       
-      <div className="gifs-container">
-        {
-          mockGifs.map((gif) => (
-            <div key={ gif.id} className="gif-card">
-              <img src={ gif.url } alt={gif.title} />
-              <h3>{ gif.title }</h3>
-              <p>
-                { gif.width }x{ gif.height } (1.5mb)
-              </p>
-            </div>
-          ))
-        }
-      </div>
-        
     </>
   )
 }
